2b86c7de326c72dc5d49afd9060e21d25cd49e32
Nilanchal Jena <nilanchaljena2702@gmail.com>
2026-07-16 17:31:25 +0530

    Practice Question Solutions

---
+++ 01. CoreJava/01. Basics/Practice/06. Count Down with Do-While.java
@@ -0,0 +1,13 @@
+class CountDown {
+    public static void main(String A[]) {
+        int n = 10;
+        do 
+        {
+            System.out.println(n);
+            n--;
+        }
+        while (n  > 0);
+
+        System.out.println("Blast Off!");
+    }
+}
